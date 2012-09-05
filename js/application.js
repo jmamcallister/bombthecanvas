6c0e051457453fddb3125f30d63b9cc94c8621a3
Alex McAllister <jmamcallister@gmail.com>
2012-09-05 23:52:59 +0100

    Begin terrain definition

--- js/application.js
+++ js/application.js
@@ -4,11 +4,19 @@
  * His name is Robert Poulson etc
  */
 
+/*
+ * Draw the terrain:
+ *	- Pick a point on the y-axis on the extreme left margin
+ *	- Set a maximum difference of y, i.e., the maximum incline
+ *	- Start adding pixel locations to data structure which are adjacent but in either direction, i.e., either +y or -y
+ *	- when calling draw(), can reference data structure (array?) on each iteration
+ */
 var ctx;
 var WIDTH, HEIGHT;
 var LEGEND_OFFSET = 100;
 var TEXT_MARGIN = 5;
 var currentInterval, intervalId;
+var terrain = [];
 
 function init() {
 	ctx = $('#canvas')[0].getContext('2d');
@@ -34,6 +42,20 @@ function drawLegend() {
 	ctx.font = '12px sans-serif';
 	ctx.textBaseline = 'top';
 	ctx.fillText('Player N', TEXT_MARGIN, (HEIGHT - LEGEND_OFFSET) + TEXT_MARGIN);
+}
+
+function initTerrain() {
+	var startX = pickStartingX();
+	var maxIncline = pickMaxIncline();
+
+}
+
+function pickStartingX() {
+
+}
+
+function pickMaxIncline() {
+
 }
 
 $(function() {
